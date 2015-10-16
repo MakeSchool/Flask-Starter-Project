3e5ea768f5cf903f53eacad61f6c26666f7ef8cd
Daniel Haaser <thedapperdan@gmail.com>
2015-10-15 17:16:10 -0700

    fix long comment

--- server.py
+++ server.py
@@ -48,6 +48,7 @@ def output_json(data, code, headers=None):
     return resp
 
 if __name__ == '__main__':
-    # Turn this on in debug mode to get detailled information about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
+    # Turn this on in debug mode to get detailed information
+    # about request related exceptions: http://flask.pocoo.org/docs/0.10/config/
     app.config['TRAP_BAD_REQUEST_ERRORS'] = True
     app.run(debug=True)
